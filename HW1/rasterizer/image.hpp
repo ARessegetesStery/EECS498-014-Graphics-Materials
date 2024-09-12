3ce571a220e52a5fc44cf0930ed312fff0d186ff
swesik <swesik@umich.edu>
2024-09-12 15:49:49 -0400

    finished part 1

--- HW1/rasterizer/image.hpp
+++ HW1/rasterizer/image.hpp
@@ -2,13 +2,12 @@
 #define ImageBuffer_H
 
 #include <algorithm>
-#include <string>
 #include <optional>
+#include <string>
 
 #include "../thirdparty/glm/glm.hpp"
 
-class Color
-{
+class Color {
 public:
     unsigned char r, g, b, a;
 
@@ -22,44 +21,34 @@ public:
     Color(const Color&);
 
     // Assignments and equality judgement
-    Color& operator= (const Color&);
-    bool operator== (const Color&);
-    bool operator!= (const Color&);
-    const char operator[] (size_t index) const;
+    Color& operator=(const Color&);
+    bool operator==(const Color&);
+    bool operator!=(const Color&);
+    const char operator[](size_t index) const;
 
     // Clearing colors
     static Color White;
     static Color Black;
 };
 
-inline Color operator+ (const Color& c1, const Color& c2)
-{
-    return Color(
-        std::clamp(static_cast<float>(c1.r) + static_cast<float>(c2.r), 0.f, 255.f),
-        std::clamp(static_cast<float>(c1.g) + static_cast<float>(c2.g), 0.f, 255.f),
-        std::clamp(static_cast<float>(c1.b) + static_cast<float>(c2.b), 0.f, 255.f),
-        static_cast<float>(c1.a)
-    );
+inline Color operator+(const Color& c1, const Color& c2) {
+    return Color(std::clamp(static_cast<float>(c1.r) + static_cast<float>(c2.r), 0.f, 255.f),
+                 std::clamp(static_cast<float>(c1.g) + static_cast<float>(c2.g), 0.f, 255.f),
+                 std::clamp(static_cast<float>(c1.b) + static_cast<float>(c2.b), 0.f, 255.f), static_cast<float>(c1.a));
 }
 
-inline Color operator* (float coeff, const Color& c)
-{
-    return Color(
-        std::clamp(static_cast<float>(c.r) * coeff, 0.f, 255.f),
-        std::clamp(static_cast<float>(c.g) * coeff, 0.f, 255.f),
-        std::clamp(static_cast<float>(c.b) * coeff, 0.f, 255.f),
-        static_cast<float>(c.a)
-    );
+inline Color operator*(float coeff, const Color& c) {
+    return Color(std::clamp(static_cast<float>(c.r) * coeff, 0.f, 255.f),
+                 std::clamp(static_cast<float>(c.g) * coeff, 0.f, 255.f),
+                 std::clamp(static_cast<float>(c.b) * coeff, 0.f, 255.f), static_cast<float>(c.a));
 }
 
-inline Color operator* (const Color& c, float coeff)
-{
+inline Color operator*(const Color& c, float coeff) {
     return coeff * c;
 }
 
-template<typename T>
-class ImageBuffer
-{
+template <typename T>
+class ImageBuffer {
 private:
     uint32_t width, height;
     T* canvas;
@@ -72,7 +61,7 @@ public:
     ImageBuffer(const ImageBuffer&);
     ~ImageBuffer();
 
-    ImageBuffer& operator= (const ImageBuffer&);
+    ImageBuffer& operator=(const ImageBuffer&);
 
     // Set/Get color for a specific pixel
     //     Attempting to set color to an invalid pixel will result in no change in the canvas
@@ -90,78 +79,64 @@ public:
 using Image = ImageBuffer<Color>;
 using ImageGrey = ImageBuffer<float>;
 
-template<typename T>
-ImageBuffer<T>::ImageBuffer(std::string filename)
-{
+template <typename T>
+ImageBuffer<T>::ImageBuffer(std::string filename) {
     this->width = 100;
     this->height = 100;
     this->canvas = new T[100 * 100];
     this->filename = filename;
 }
 
-template<typename T>
-ImageBuffer<T>::ImageBuffer(unsigned int w, unsigned int h, std::string filename)
-{
-    if (w > 2000)
-        w = 2000;
-    if (h > 2000)
-        h = 2000;
+template <typename T>
+ImageBuffer<T>::ImageBuffer(unsigned int w, unsigned int h, std::string filename) {
+    if (w > 2000) w = 2000;
+    if (h > 2000) h = 2000;
     this->width = w;
     this->height = h;
     this->canvas = new T[static_cast<size_t>(w) * static_cast<size_t>(h)];
     this->filename = filename;
 }
 
-template<>
+template <>
 ImageBuffer<Color>::ImageBuffer(std::string filename);
 
-template<>
+template <>
 ImageBuffer<Color>::ImageBuffer(std::string filename);
 
-template<>
+template <>
 ImageBuffer<Color>::ImageBuffer(unsigned int w, unsigned int h, std::string filename);
 
-template<typename T>
-ImageBuffer<T>::ImageBuffer(const ImageBuffer<T>& image)
-{
+template <typename T>
+ImageBuffer<T>::ImageBuffer(const ImageBuffer<T>& image) {
     *this = image;
 }
 
-template<typename T>
-ImageBuffer<T>::~ImageBuffer()
-{
-    if (canvas)
-        delete[] canvas;
+template <typename T>
+ImageBuffer<T>::~ImageBuffer() {
+    if (canvas) delete[] canvas;
 }
 
-template<typename T>
-ImageBuffer<T>& ImageBuffer<T>::operator= (const ImageBuffer<T>& image)
-{
-    if (this->canvas)
-        delete[] canvas;
+template <typename T>
+ImageBuffer<T>& ImageBuffer<T>::operator=(const ImageBuffer<T>& image) {
+    if (this->canvas) delete[] canvas;
 
     this->width = image.width;
     this->height = image.height;
     this->canvas = new T[image.width * image.height];
-    for (unsigned int i = 0; i != image.width * image.height; ++i)
-        this->canvas[i] = image.canvas[i];
-    this->filename = filename;
+    for (unsigned int i = 0; i != image.width * image.height; ++i) this->canvas[i] = image.canvas[i];
+    this->filename = image.filename;
 
     return *this;
 }
 
-template<typename T>
-void ImageBuffer<T>::Set(unsigned int w, unsigned int h, T c)
-{
-    if (!(!canvas || w >= width || h >= height))
-        this->canvas[(size_t)(h * this->width + w)] = c;
+template <typename T>
+void ImageBuffer<T>::Set(unsigned int w, unsigned int h, T c) {
+    if (!(!canvas || w >= width || h >= height)) this->canvas[(size_t) (h * this->width + w)] = c;
 }
 
-template<typename T>
-std::optional<T> ImageBuffer<T>::Get(unsigned int w, unsigned int h) const
-{
-    if (!(!canvas || w >= width || h >= height))
-        return this->canvas[(size_t)(h * this->width + w)];
+template <typename T>
+std::optional<T> ImageBuffer<T>::Get(unsigned int w, unsigned int h) const {
+    if (!(!canvas || w >= width || h >= height)) return this->canvas[(size_t) (h * this->width + w)];
     return std::nullopt;
 }
 
--- HW1/rasterizer/rasterizer_impl.cpp
+++ HW1/rasterizer/rasterizer_impl.cpp
@@ -1,37 +1,74 @@
 #include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <random>
 
 #include "image.hpp"
 #include "loader.hpp"
 #include "rasterizer.hpp"
 
 // TODO
-void Rasterizer::DrawPixel(uint32_t x, uint32_t y, Triangle trig, AntiAliasConfig config, uint32_t spp, Image& image, Color color)
-{
-    if (config == AntiAliasConfig::NONE)            // if anti-aliasing is off
+
+void Rasterizer::DrawPixel(uint32_t x, uint32_t y, Triangle trig, AntiAliasConfig config, uint32_t spp, Image& image,
+                           Color color) {
+    glm::vec3 pixel(x, y, 0);
+    if (config == AntiAliasConfig::NONE)   // if anti-aliasing is off
     {
+        glm::vec3 X = pixel + glm::vec3(0.5, 0.5, 0);
+        glm::vec3 a1(trig.pos[0]);
+        glm::vec3 a2(trig.pos[1]);
+        glm::vec3 a3(trig.pos[2]);
+        glm::vec3 s1 = glm::cross(X - a1, a2 - a1);
+        glm::vec3 s2 = glm::cross(X - a2, a3 - a2);
+        glm::vec3 s3 = glm::cross(X - a3, a1 - a3);
+        glm::vec3 screen_normal = glm::vec3(0, 0, 1);
 
+
+        if ((glm::dot(s1, screen_normal) > 0 && glm::dot(s2, screen_normal) > 0 && glm::dot(s3, screen_normal) > 0)
+            || (glm::dot(s1, screen_normal) < 0 && glm::dot(s2, screen_normal)) < 0
+                 && glm::dot(s3, screen_normal) < 0) {
+            // std::cout << "set pixel (" << x << ", " << y << ")" << std::endl;
+            image.Set(x, y, color);
+            return;
+        }
+
+    } else if (config == AntiAliasConfig::SSAA) {   // if anti-aliasing is on
+        uint32_t num_inside = 0;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0.0, 1.0);
+        for (uint32_t i = 0; i < spp; i++) {
+            glm::vec3 X = pixel + glm::vec3(dis(gen), dis(gen), 0);
+            glm::vec3 a1(trig.pos[0]);
+            glm::vec3 a2(trig.pos[1]);
+            glm::vec3 a3(trig.pos[2]);
+            glm::vec3 s1 = glm::cross(X - a1, a2 - a1);
+            glm::vec3 s2 = glm::cross(X - a2, a3 - a2);
+            glm::vec3 s3 = glm::cross(X - a3, a1 - a3);
+            glm::vec3 screen_normal = glm::vec3(0, 0, 1);
+
+
+            if ((glm::dot(s1, screen_normal) > 0 && glm::dot(s2, screen_normal) > 0 && glm::dot(s3, screen_normal) > 0)
+                || (glm::dot(s1, screen_normal) < 0 && glm::dot(s2, screen_normal) < 0
+                    && glm::dot(s3, screen_normal) < 0)) {
+                num_inside++;
+            }
+        }
+        color = ((double) num_inside / (double) spp) * color;
+        image.Set(x, y, color);
+        return;
     }
-    else if (config == AntiAliasConfig::SSAA)       // if anti-aliasing is on
-    {
-
-    }
-
-    // if the pixel is inside the triangle
-    image.Set(x, y, color);
-
     return;
 }
 
 // TODO
-void Rasterizer::AddModel(MeshTransform transform, glm::mat4 rotation)
-{
+void Rasterizer::AddModel(MeshTransform transform, glm::mat4 rotation) {
     /* model.push_back( model transformation constructed from translation, rotation and scale );*/
     return;
 }
 
 // TODO
-void Rasterizer::SetView()
-{
+void Rasterizer::SetView() {
     const Camera& camera = this->loader.GetCamera();
     glm::vec3 cameraPos = camera.pos;
     glm::vec3 cameraLookAt = camera.lookAt;
@@ -43,16 +80,15 @@ void Rasterizer::SetView()
 }
 
 // TODO
-void Rasterizer::SetProjection()
-{
+void Rasterizer::SetProjection() {
     const Camera& camera = this->loader.GetCamera();
 
-    float nearClip = camera.nearClip;                   // near clipping distance, strictly positive
-    float farClip = camera.farClip;                     // far clipping distance, strictly positive
-    
+    float nearClip = camera.nearClip;   // near clipping distance, strictly positive
+    float farClip = camera.farClip;     // far clipping distance, strictly positive
+
     float width = this->loader.GetWidth();
     float height = this->loader.GetHeight();
-    
+
     // TODO change this line to the correct projection matrix
     this->projection = glm::mat4(1.);
 
@@ -60,8 +96,7 @@ void Rasterizer::SetProjection()
 }
 
 // TODO
-void Rasterizer::SetScreenSpace()
-{
+void Rasterizer::SetScreenSpace() {
     float width = this->loader.GetWidth();
     float height = this->loader.GetHeight();
 
@@ -72,8 +107,7 @@ void Rasterizer::SetScreenSpace()
 }
 
 // TODO
-glm::vec3 Rasterizer::BarycentricCoordinate(glm::vec2 pos, Triangle trig)
-{
+glm::vec3 Rasterizer::BarycentricCoordinate(glm::vec2 pos, Triangle trig) {
     return glm::vec3();
 }
 
@@ -81,9 +115,8 @@ glm::vec3 Rasterizer::BarycentricCoordinate(glm::vec2 pos, Triangle trig)
 float Rasterizer::zBufferDefault = float();
 
 // TODO
-void Rasterizer::UpdateDepthAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, ImageGrey& ZBuffer)
-{
-
+void Rasterizer::UpdateDepthAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed,
+                                    ImageGrey& ZBuffer) {
     float result;
     ZBuffer.Set(x, y, result);
 
@@ -91,9 +124,7 @@ void Rasterizer::UpdateDepthAtPixel(uint32_t x, uint32_t y, Triangle original, T
 }
 
 // TODO
-void Rasterizer::ShadeAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, Image& image)
-{
-
+void Rasterizer::ShadeAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, Image& image) {
     Color result;
     image.Set(x, y, result);
 
