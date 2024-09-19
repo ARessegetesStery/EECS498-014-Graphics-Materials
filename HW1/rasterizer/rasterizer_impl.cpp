e40a36cd6cfde2c74c1ec0617851fcc7eb60de59
swesik <swesik@umich.edu>
2024-09-19 16:20:35 -0400

    finished project

--- HW1/rasterizer/rasterizer_impl.cpp
+++ HW1/rasterizer/rasterizer_impl.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <random>
+#include <limits>
+#include <_types/_uint32_t.h>
 
 #include "image.hpp"
 #include "loader.hpp"
@@ -11,25 +13,37 @@
 // TODO
 const glm::vec3 pixel_center = glm::vec3(0.5F, 0.5F, 0.0F);
 
+void printMat4(glm::mat4 mat){
+    mat = glm::transpose(mat);
+    // std::cout << "matrix" << std::endl;
+    std::cout << mat[0][0] << "\t" << mat[0][1] << "\t" << mat[0][2] << "\t" << mat[0][3] << std::endl;
+    std::cout << mat[1][0] << "\t" << mat[1][1] << "\t" << mat[1][2] << "\t" << mat[1][3] << std::endl;
+    std::cout << mat[2][0] << "\t" << mat[2][1] << "\t" << mat[2][2] << "\t" << mat[2][3] << std::endl;
+    std::cout << mat[3][0] << "\t" << mat[3][1] << "\t" << mat[3][2] << "\t" << mat[3][3] << std::endl;
+}
+
+bool sampleIsInsideTriangle(glm::vec3 sample_pos, Triangle trig){
+    glm::vec3 a1(trig.pos[0].x, trig.pos[0].y, 0.0F);
+    glm::vec3 a2(trig.pos[1].x, trig.pos[1].y, 0.0F);
+    glm::vec3 a3(trig.pos[2].x, trig.pos[2].y, 0.0F);
+    glm::vec3 s1 = glm::cross(sample_pos - a1, a2 - a1);
+    glm::vec3 s2 = glm::cross(sample_pos - a2, a3 - a2);
+    glm::vec3 s3 = glm::cross(sample_pos - a3, a1 - a3);
+    glm::vec3 screen_normal = glm::vec3(0, 0, 1);
+
+    return (glm::dot(s1, screen_normal) > 0.0F && glm::dot(s2, screen_normal) > 0.0F
+                && glm::dot(s3, screen_normal) > 0.0F)
+            || (glm::dot(s1, screen_normal) < 0.0F && glm::dot(s2, screen_normal) < 0.0F
+                && glm::dot(s3, screen_normal) < 0.0F);
+}
+
 void Rasterizer::DrawPixel(uint32_t x, uint32_t y, Triangle trig, AntiAliasConfig config, uint32_t spp, Image& image,
                            Color color) {
     glm::vec3 pixel(x, y, 0.0F);
     if (config == AntiAliasConfig::NONE)   // if anti-aliasing is off
     {
         glm::vec3 sample_pos = pixel + pixel_center;
-        glm::vec3 a1(trig.pos[0]);
-        glm::vec3 a2(trig.pos[1]);
-        glm::vec3 a3(trig.pos[2]);
-        glm::vec3 s1 = glm::cross(sample_pos - a1, a2 - a1);
-        glm::vec3 s2 = glm::cross(sample_pos - a2, a3 - a2);
-        glm::vec3 s3 = glm::cross(sample_pos - a3, a1 - a3);
-        glm::vec3 screen_normal = glm::vec3(0, 0, 1);
-
-        if ((glm::dot(s1, screen_normal) > 0.0F && glm::dot(s2, screen_normal) > 0.0F
-             && glm::dot(s3, screen_normal) > 0.0F)
-            || (glm::dot(s1, screen_normal) < 0.0F && glm::dot(s2, screen_normal) < 0.0F
-                && glm::dot(s3, screen_normal) < 0.0F)) {
-            // std::cout << "set pixel (" << x << ", " << y << ")" << std::endl;
+        if (sampleIsInsideTriangle(sample_pos, trig)) {
             image.Set(x, y, color);
             return;
         }
@@ -41,18 +55,8 @@ void Rasterizer::DrawPixel(uint32_t x, uint32_t y, Triangle trig, AntiAliasConfi
         std::uniform_real_distribution<> dis(0.0F, 1.0);
         for (uint32_t i = 0; i < spp; i++) {
             glm::vec3 x = pixel + glm::vec3(dis(gen), dis(gen), 0);
-            glm::vec3 a1(trig.pos[0]);
-            glm::vec3 a2(trig.pos[1]);
-            glm::vec3 a3(trig.pos[2]);
-            glm::vec3 s1 = glm::cross(x - a1, a2 - a1);
-            glm::vec3 s2 = glm::cross(x - a2, a3 - a2);
-            glm::vec3 s3 = glm::cross(x - a3, a1 - a3);
-            glm::vec3 screen_normal = glm::vec3(0, 0, 1);
 
-
-            if ((glm::dot(s1, screen_normal) > 0 && glm::dot(s2, screen_normal) > 0 && glm::dot(s3, screen_normal) > 0)
-                || (glm::dot(s1, screen_normal) < 0 && glm::dot(s2, screen_normal) < 0
-                    && glm::dot(s3, screen_normal) < 0)) {
+            if (sampleIsInsideTriangle(x, trig)) {
                 num_inside++;
             }
         }
@@ -78,6 +82,7 @@ void Rasterizer::AddModel(MeshTransform transform, glm::mat4 rotation) {
     );
 
     model.push_back(translation * rotation * scale);
+    // printMat4(model.back());
 }
 
 // TODO
@@ -103,6 +108,7 @@ void Rasterizer::SetView() {
                             0.0F, 0.0F, 0.0F, 1.0F                               // fourth column
     );
     this->view = model_to_view * translate_to_origin;
+    // printMat4(this->view);
 }
 
 // TODO
@@ -136,6 +142,7 @@ void Rasterizer::SetProjection() {
     );
 
     this->projection = scale_to_bounding_box * tranlsate_to_center * perspective;
+    // printMat4(this->projection);
 }
 
 // TODO
@@ -149,31 +156,76 @@ void Rasterizer::SetScreenSpace() {
                                   0.0F, 0.0F, 1.0F, 0.0F, // third column
                                   width/2, height/2, 0.0F, 1.0F // fourth column
     );
-
-    // this->screenspace = glm::mat4(1.0F);
+    // printMat4(this->screenspace);
 }
 
 // TODO
 glm::vec3 Rasterizer::BarycentricCoordinate(glm::vec2 pos, Triangle trig) {
-    return glm::vec3();
+
+    // glm::vec3 pos3 = glm::vec3(pos, 1);
+    // glm::mat3x3 A = glm::mat3x3(
+    //     trig.pos[0].x, trig.pos[1].x, trig.pos[2].x,
+    //     trig.pos[0].y, trig.pos[1].y, trig.pos[2].y,
+    //     1, 1, 1
+    // );
+    // return glm::inverse(glm::transpose(A)) * pos3;
+
+    float alpha = (-(pos.x - trig.pos[1].x) * (trig.pos[2].y - trig.pos[1].y) + (pos.y - trig.pos[1].y) * (trig.pos[2].x - trig.pos[1].x)) / 
+                    ( -(trig.pos[0].x - trig.pos[1].x) * (trig.pos[2].y - trig.pos[1].y) + (trig.pos[0].y - trig.pos[1].y) * (trig.pos[2].x - trig.pos[1].x));
+    float beta = (-(pos.x - trig.pos[2].x) * (trig.pos[0].y - trig.pos[2].y) + (pos.y - trig.pos[2].y) * (trig.pos[0].x - trig.pos[2].x)) / 
+                    ( -(trig.pos[1].x - trig.pos[2].x) * (trig.pos[0].y - trig.pos[2].y) + (trig.pos[1].y - trig.pos[2].y) * (trig.pos[0].x - trig.pos[2].x));
+    float gamma = 1 - alpha - beta;
+    return glm::vec3(alpha, beta, gamma);
 }
 
 // TODO
-float Rasterizer::zBufferDefault = float();
+float Rasterizer::zBufferDefault = -1.0F;
 
 // TODO
 void Rasterizer::UpdateDepthAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed,
                                     ImageGrey& ZBuffer) {
-    float result;
-    ZBuffer.Set(x, y, result);
 
-    return;
+    glm::vec2 pos(static_cast<float>(x), static_cast<float>(y));
+    glm::vec3 bary_coord = BarycentricCoordinate(pos,transformed);
+
+    if(bary_coord.x < 0 || bary_coord.y < 0 || bary_coord.z < 0){
+        return;
+    }
+    float inv_result = bary_coord.x * (1/transformed.pos[0].z) + bary_coord.y * (1/transformed.pos[1].z) + bary_coord.z * (1/transformed.pos[2].z);
+    if(1/inv_result > ZBuffer.Get(x, y)){
+        ZBuffer.Set(x, y, 1/inv_result);
+    }
 }
 
 // TODO
 void Rasterizer::ShadeAtPixel(uint32_t x, uint32_t y, Triangle original, Triangle transformed, Image& image) {
-    Color result;
-    image.Set(x, y, result);
 
-    return;
+    glm::vec2 pos(static_cast<float>(x), static_cast<float>(y));
+    glm::vec3 bary_coord = BarycentricCoordinate(pos,transformed);
+
+
+    float inv_depth = bary_coord.x * (1/transformed.pos[0].z) + bary_coord.y * (1/transformed.pos[1].z) + bary_coord.z * (1/transformed.pos[2].z);
+    if(ZBuffer.Get(x, y) != 1/inv_depth){
+        return;
+    }
+
+    Color result = loader.GetAmbientColor();;
+    const std::vector<Light>& lights = loader.GetLights();
+    const Camera& camera = loader.GetCamera();
+    glm::vec3 norm = glm::normalize(bary_coord.x * original.normal[0] + bary_coord.y * original.normal[1] + bary_coord.z * original.normal[2]);
+    for(auto& light : lights){
+        glm::vec3 world_coord = bary_coord.x * original.pos[0] + bary_coord.y * original.pos[1] + bary_coord.z * original.pos[2];
+        glm::vec3 light_ray = light.pos - world_coord;
+        glm::vec3 reflection_ray = camera.pos - world_coord;
+        glm::vec3 half_vec = glm::normalize(light_ray + reflection_ray);
+        
+        float r = glm::length(light_ray);
+        Color diffuse = light.intensity * (1/ (r*r)) * std::max(0.0F,glm::dot(light_ray, norm)) * light.color;
+        Color specular = light.intensity * (1/ (r*r)) *std::pow(std::max(0.0F,glm::dot(half_vec, norm)),loader.GetSpecularExponent()) * light.color;
+        result = result + diffuse + specular;
+
+        // result = result + diffuse;
+    }
+
+    image.Set(x, y, result);
 }
--- HW1/rasterizer/renderer.cpp
+++ HW1/rasterizer/renderer.cpp
@@ -1,3 +1,5 @@
+#include "renderer.hpp"
+
 #include <cstdint>
 #include <iostream>
 #include <string>
@@ -5,49 +7,37 @@
 #include "image.hpp"
 #include "loader.hpp"
 #include "rasterizer.hpp"
-#include "renderer.hpp"
 
-void PrintTask(const Loader& loader)
-{
+void PrintTask(const Loader& loader) {
     std::string sephead = "======================Config======================\n";
     std::string sep = "==================================================\n";
     std::string msg = "Running task with the following configuration:\n" + sephead + loader.Info() + sep;
     std::cout << msg;
 }
 
-void PrintTaskTriangle(const Triangle& trig)
-{
+void PrintTaskTriangle(const Triangle& trig) {
     std::string sephead = "=====================Triangle=====================\n";
     std::string sep = "==================================================\n";
-    std::string msg = sephead + 
-        "Vertex 1 position: " + ToStr(trig.pos[0]) + "\n" +
-        "Vertex 2 position: " + ToStr(trig.pos[1]) + "\n" +
-        "Vertex 3 position: " + ToStr(trig.pos[2]) + "\n"
-        + sep;
+    std::string msg = sephead + "Vertex 1 position: " + ToStr(trig.pos[0]) + "\n" + "Vertex 2 position: "
+                    + ToStr(trig.pos[1]) + "\n" + "Vertex 3 position: " + ToStr(trig.pos[2]) + "\n" + sep;
     std::cout << msg;
 }
 
-void PrintTaskTransformTest(const glm::vec3 input, const glm::vec4 output, const glm::vec3 expected)
-{
+void PrintTaskTransformTest(const glm::vec3 input, const glm::vec4 output, const glm::vec3 expected) {
     std::string sephead = "===============Task: Transform Test===============\n";
     std::string sep = "==================================================\n";
     glm::vec4 expected4(expected, 1);
     glm::vec4 normalizedOutput = output / output.w;
-    std::string msg = sephead + 
-        "Input: " + ToStr(input) + "\n" +
-        "Output: " + ToStr(normalizedOutput) + "\n" +
-        "Expected: " + ToStr(expected4) + "\n"
-        + sep;
+    std::string msg = sephead + "Input: " + ToStr(input) + "\n" + "Output: " + ToStr(normalizedOutput) + "\n"
+                    + "Expected: " + ToStr(expected4) + "\n" + sep;
     std::cout << msg;
 }
 
-void Renderer::Render(int argc, char** argv)
-{
+void Renderer::Render(int argc, char** argv) {
     std::string modelName;
     std::string yamlConfigName = "config.yaml";
 
-    if (argc != 1)
-    {
+    if (argc != 1) {
         yamlConfigName = argv[1];
         std::cout << "using customized config name" << yamlConfigName << std::endl;
     }
@@ -55,38 +45,26 @@ void Renderer::Render(int argc, char** argv)
     Loader loader(yamlConfigName);
     bool success = loader.Load();
 
-    if (success)
-    {
+    if (success) {
         PrintTask(loader);
         Image image(loader.GetWidth(), loader.GetHeight());
 
         Rasterizer rasterizer(loader);
 
-        glm::mat4x4 viewxprojection{
-            1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1
-        };
+        glm::mat4x4 viewxprojection { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
 
-        if (loader.GetType() == TestType::TRIANGLE)
-        {
+        if (loader.GetType() == TestType::TRIANGLE) {
             // notice that glm::mat4x4 is column-major, so the actual matrix is the transpose of the matrix read off
             uint32_t halfWidth = loader.GetWidth() / 2;
             uint32_t halfHeight = loader.GetHeight() / 2;
-            viewxprojection = glm::mat4x4{
-                halfWidth, 0         , 0, 0,
-                0        , halfHeight, 0, 0, 
-                0        , 0         , 0, 0,             // discard z values
-                halfWidth, halfHeight, 0, 1
-            };
-            rasterizer.model.push_back(glm::mat4x4(1.0f));      // Add an identity model matrix to avoid special judgement below
-        }
-        else
-        {
+            viewxprojection
+              = glm::mat4x4 { halfWidth, 0,          0, 0, 0, halfHeight, 0, 0, 0, 0, 0, 0,   // discard z values
+                              halfWidth, halfHeight, 0, 1 };
+            rasterizer.model.push_back(
+              glm::mat4x4(1.0f));   // Add an identity model matrix to avoid special judgement below
+        } else {
             // First load the matrices to the rasterizer
-            for (size_t index = 0; index != loader.GetTransforms().size(); ++index)
-            {
+            for (size_t index = 0; index != loader.GetTransforms().size(); ++index) {
                 MeshTransform transform = loader.GetTransforms()[index];
                 rasterizer.AddModel(transform);
             }
@@ -98,22 +76,18 @@ void Renderer::Render(int argc, char** argv)
             // Compose the matrices
             viewxprojection = rasterizer.screenspace * rasterizer.projection * rasterizer.view;
         }
-        
+
         // If this is test on transforms, then do not need to iterate over the meshes
-        if (loader.GetType() == TestType::TRANSFORM_TEST)
-        {
+        if (loader.GetType() == TestType::TRANSFORM_TEST) {
             glm::vec3 input = loader.GetTestInput();
             glm::vec3 expected = loader.GetTestExpected();
             glm::vec4 input4(input, 1);
 
-            if (rasterizer.model.size() == 0)
-                throw std::runtime_error("No model matrix specified for transform test");
+            if (rasterizer.model.size() == 0) throw std::runtime_error("No model matrix specified for transform test");
 
             glm::vec4 output = viewxprojection * rasterizer.model[0] * input4;
             PrintTaskTransformTest(input, output, expected);
-        }
-        else 
-        {
+        } else {
             auto& shapes = loader.GetShapes();
             auto& attribs = loader.GetAttribs();
 
@@ -122,12 +96,10 @@ void Renderer::Render(int argc, char** argv)
 
             std::vector<Triangle> transformedTrigs;
             std::vector<Triangle> originalTrigs;
-            
+
             const size_t fv = 3;
-            for (size_t s = 0; s < shapes.size(); s++) 
-            {
-                if (loader.GetType() == TestType::SHADING)
-                {
+            for (size_t s = 0; s < shapes.size(); s++) {
+                if (loader.GetType() == TestType::SHADING) {
                     transformedTrigs.clear();
                     originalTrigs.clear();
                     transformedTrigs.reserve(shapes.size());
@@ -136,23 +108,20 @@ void Renderer::Render(int argc, char** argv)
 
                 // Loop over faces(polygon)
                 size_t index_offset = 0;
-                for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) 
-                {
+                for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
                     // Loop over vertices in the face.
                     Triangle transformed, original;
-                    for (size_t v = 0; v < fv; v++) 
-                    {
+                    for (size_t v = 0; v < fv; v++) {
                         // access to vertex
                         tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
                         tinyobj::real_t vx = attribs.vertices[3 * size_t(idx.vertex_index) + 0];
                         tinyobj::real_t vy = attribs.vertices[3 * size_t(idx.vertex_index) + 1];
                         tinyobj::real_t vz = attribs.vertices[3 * size_t(idx.vertex_index) + 2];
                         glm::vec4 vec(vx, vy, vz, 1);
-                        
+
                         // init to identity so that the program will no crash even without model matrices being added
                         glm::mat4 modelMat = glm::mat4(1.f);
-                        if (rasterizer.model.size() > s)
-                            modelMat = rasterizer.model[s];
+                        if (rasterizer.model.size() > s) modelMat = rasterizer.model[s];
 
                         if (loader.GetType() == TestType::TRIANGLE)
                             transformed.pos[v] = viewxprojection * vec;
@@ -161,8 +130,7 @@ void Renderer::Render(int argc, char** argv)
 
                         original.pos[v] = modelMat * vec;
 
-                        if (idx.normal_index >= 0) 
-                        {
+                        if (idx.normal_index >= 0) {
                             tinyobj::real_t nx = attribs.normals[3 * size_t(idx.normal_index) + 0];
                             tinyobj::real_t ny = attribs.normals[3 * size_t(idx.normal_index) + 1];
                             tinyobj::real_t nz = attribs.normals[3 * size_t(idx.normal_index) + 2];
@@ -180,9 +148,8 @@ void Renderer::Render(int argc, char** argv)
                         rasterizer.DrawPrimitiveRaw(image, transformed, loader.GetAntiAliasConfig(), loader.GetSpp());
                     else if (loader.GetType() == TestType::SHADING_DEPTH || loader.GetType() == TestType::SHADING)
                         rasterizer.DrawPrimitiveDepth(transformed, original, rasterizer.ZBuffer);
-                    
-                    if (loader.GetType() == TestType::SHADING)
-                    {
+
+                    if (loader.GetType() == TestType::SHADING) {
                         transformedTrigs.push_back(transformed);
                         originalTrigs.push_back(original);
                     }
