84c38e9be687427a26cbbd3d717a1379b5cfc708
Junwen Yu <142624005+AnemoCider@users.noreply.github.com>
2024-09-20 17:30:05 -0400

    Update config
    Deprecate DEBUG config

--- HW2/Config.h
+++ HW2/Config.h
@@ -4,7 +4,7 @@
 
 #include <string_view>
 
-constexpr bool DEBUG = false;
+
 constexpr int SPP = 16;
 
 constexpr int SEED = 42;
@@ -15,3 +15,5 @@ constexpr float RR = 0.8f;
 constexpr std::string_view OBJ_PATH = "./models/cornellBox/CornellBox-Original.obj";
 constexpr std::string_view MTL_SEARCH_DIR = "./models/cornellBox/";
 constexpr std::string_view OUTPUT_PATH = "./binary.ppm";
+
+constexpr bool DEBUG = false; // reserved for future use. Do not enable it currently.
